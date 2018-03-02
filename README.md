7967c69fb427abf31c5af60d98873cd85164519f
Alex Panchuk <apanchuk@gmail.com>
2018-03-02 21:54:51 +0500

    applying seatch during user input

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ yarn start - run dev server on localhost:3000
 
 ### TODO
 
+* ~~applying search during user input~~
 * move dispatch out of try-catch
 * optimize Phones and Categories first rendering
 * apply components with "Presentational and Container components pattern"
 * migrate to react-router 4
-* applying search during user input
 * migrate to redux-saga
 * improve project folder architecture
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -5,41 +5,27 @@ import { searchPhone } from "../../actions";
 class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: ""
-    };
-
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.input.focus();
   }
 
   handleChange(event) {
-    this.setState({
-      value: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.searchPhone(this.state.value);
+    this.props.searchPhone(event.target.value);
   }
 
   render() {
     return (
       <div className="well blosd">
         <h3 className="lead">Quick search</h3>
-        <form onSubmit={this.handleSubmit} className="input-group">
-          <input
-            onChange={this.handleChange}
-            type="text"
-            className="form-control"
-          />
-          <span className="input-group-btn">
-            <button type="submit" className="btn btn-default">
-              <span className="glyphicon glyphicon-search" />
-            </button>
-          </span>
-        </form>
+        <input
+          onChange={this.handleChange}
+          type="text"
+          className="form-control"
+          ref={node => (this.input = node)}
+        />
       </div>
     );
   }
